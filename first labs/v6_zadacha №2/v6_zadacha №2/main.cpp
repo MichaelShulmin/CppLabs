1b75d4273ce1e603edabf8496c1957e52cacd13d
Unknown <m.shulmin@mail.ru>
2017-09-24 17:08:32 +0500

    Даны действительные положительные числа a, b, c, x, y. Выяснить, пройдет ли кирпич с ребрами a, b, c в прямоугольное отверстие со сторонами x и y. Просовывать кирпич разрешается только так, чтобы каждое из его ребер было параллельно или перпендикулярно каждой из сторон отверстия.

---
+++ first labs/v6_zadacha №2/v6_zadacha №2/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, x, y; 
+	cout << "a" << endl;
+	cin >> a; 
+	cout << "b" << endl;
+	cin >> b; 
+	cout << "c" << endl;
+	cin >> c;
+	cout << "x" << endl;
+	cin >> x;
+	cout << "y" << endl;
+	cin >> y;
+	
+	if ((a <= x & b <= y) || (b <= x & a <= y) || (a <= x & c <= y) || (c <= x & a <= y) || (c <= x & b <= y) || (b <= x & c <= y))
+	{
+		cout << "Кирпич пройдёт в отверстие." << endl;
+	}
+
+	else if ((a >= x & b >= y) || (b >= x & a >= y) || (a >= x & c >= y) || (c >= x & a >= y) || (c >= x & b >= y) || (b >= x & c >= y))
+	{
+		cout << "Кирпич не пройдёт в отверстие." << endl;
+	}
+
+}
