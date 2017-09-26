d777565bb5f1360a2ad67c5b462767fabc38d4d9
Unknown <m.shulmin@mail.ru>
2017-09-26 20:27:41 +0500

    Дано натуральное число. Приписать к нему такое же число слева. 34=>3434

---
+++ first labs/v6_zadacha№3/v6_zadacha№3/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int i;
+	cout << "Input i" << endl;
+	cin >> i;
+	int r;
+	r = 0;
+	i != 0;
+	while (i / 10 < 0)
+	{
+		r += 1;
+	}
+	int a;
+	a = i*(pow(10, r));
+	i += a;
+	cin >> i;
+}
